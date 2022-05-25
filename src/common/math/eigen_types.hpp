3eef2a9219955272f2505432cb3684e82402ad75
akhil <akhshah@gmail.com>
2022-05-25 15:01:18 -0400

    wip integrators

---
+++ src/common/math/eigen_types.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include <Eigen/Core>
+
+namespace math {
+    using Vector2d = Eigen::Vector<double, 2>;
+    using Vector3d = Eigen::Vector<double, 3>;
+    
+    using VectorXd = Eigen::Vector<double, Dynamic>;
+} // namespace
